1b2aac3ea2eefb69baa345b391767e9d4f36dc52
jenki2ma <94399803+jenki2ma@users.noreply.github.com>
2022-10-13 22:11:18 -0400

    Initial project setup

---
+++ src/main/java/com/fitness/workoutlog/App.java
@@ -0,0 +1,13 @@
+package com.fitness.workoutlog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
